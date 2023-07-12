b0d9122366fed860b70d527b3b89b474b5278d87
Igor_Potapoff <igor.potapoff@yandex.ru>
2023-07-12 08:57:24 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Prompt(string message)
+{
+
+    Console.Write(message);
+
+    int result = Convert.ToInt32(Console.ReadLine());
+
+    return result;
+}
+
+
+bool AreNaturalElements(int m, int n)
+{
+    if (m > 0 && n > 0) return true;
+
+    else
+
+        return false;
+}
+
+
+int FindSumOfNaturals(int m, int n)
+{
+
+    if (m == n)
+    {
+
+        return n;
+
+    }
+    else return m + FindSumOfNaturals(m + 1, n);
+}
+
+
+Console.Clear();
+int m = Prompt("Input number M => ");
+int n = Prompt("Input number N => ");
+
+if (AreNaturalElements(m, n))
+
+{
+    Console.WriteLine();
+    Console.WriteLine($"Sum of natural elements between {m} and {n} is {FindSumOfNaturals(m, n)}");
+}
+
+else{
+    Console.WriteLine();
+    Console.WriteLine($"Mistake! One of the following problems detected: \n 1)  {m} or (and) {n}  is (are) not natural elements.\n 2) Element {m} is bigger than element {n}. ");
+}
+Console.WriteLine();
